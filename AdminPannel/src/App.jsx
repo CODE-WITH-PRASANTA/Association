0aa3136e300def47fe15ab748ab36e77f13ff690
Santanu <santanuranjanbal@gmail.com>
2026-07-04 13:17:18 +0530

    done

--- AdminPannel/src/App.jsx
+++ AdminPannel/src/App.jsx
@@ -6,6 +6,11 @@ import {
   Navigate,
 } from "react-router-dom";
 import MainLayout from "./Layout/MainLayout/MainLayout";
+import GallaryPosting from "./Components/GallaryPosting/GallaryPosting";
+import VideoPosting from "./Components/VideoPosting/VideoPosting";
+import EventManagement from "./Components/EventManagement/EventManagement";
+import TeamManagement from "./Components/TeamManagement/TeamManagement";
+import ContactPosting from "./Components/ContactPosting/ContactPosting";
 
 
 
@@ -16,6 +21,12 @@ function App() {
       <Routes>
 
         <Route path="/" element={<MainLayout />}>
+
+        <Route path="/gallery-management" element={<GallaryPosting />} />
+        <Route path="/video-management" element={<VideoPosting />} />
+        <Route path="/event-management" element={<EventManagement />} />
+        <Route path="/team-management" element={<TeamManagement />} />
+        <Route path="/contact-management" element={<ContactPosting />} />
 
          
         </Route>
